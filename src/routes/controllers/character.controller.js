06927b58ba12ff835dd114861e451b93b636aca0
camila <camilafkapp@gmail.com>
2022-08-11 17:25:50 -0300

    filters done

--- src/routes/controllers/character.controller.js
+++ src/routes/controllers/character.controller.js
@@ -1,116 +1,119 @@
-import {Character} from '../../models/Character.js'
-import { Movie } from '../../models/Movie.js'
+import { Character } from "../../models/Character.js";
+import { Movie } from "../../models/Movie.js";
+import { Op } from "sequelize";
 
-export const getCharacters = async (req, res) => {
-    try {
+export const getCharacters = async (req, res, next) => {
+  try {
+    const {name, age, weight} = req.query;
+    
+    const allCharacters = await Character.findAll({
+      attributes: ["name", "image"],
+    });
 
-        const {name} = req.query;
-        const allCharacters = await Character.findAll({
-            attributes: ['name', 'image']
-        })
-
-        if(name) {
-            let filteredCharacters = allCharacters.filter(character => character.name.toLowerCase().includes(name.toLowerCase()))
-            filteredCharacters.length
-            ? res.status(200).send(filteredCharacters)
-            : res.status(404).send('Error')
-        } else {
-            res.status(200).send(allCharacters)
-        }
-
-    } catch (error) {
-        return res.status(500).json({message: error.message})  
+    if (name || age || weight) {
+      const characterFiltered = await Character.findAll({
+        where: {
+          [Op.or]: [
+            { name: { [Op.like]: `%${name}%` } },
+            { age: { [Op.eq]: age } },
+            { weight: { [Op.eq]: weight } },
+          ]
+        },
+      });
+      return res.status(200).json(characterFiltered);
+    } else {
+      return res.status(200).json(allCharacters);
     }
-}
+    
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const postCharacter = async (req, res) => {
-    try {
-        let {name, image, age, weight, story, movie} = req.body
-        console.log(movie)
+  try {
+    let { name, image, age, weight, story, movie } = req.body;
+    console.log(movie);
 
-        const newCharacter = await Character.create({
-            name, image, age, weight, story
-        })
+    const newCharacter = await Character.create({
+      name,
+      image,
+      age,
+      weight,
+      story,
+    });
 
-        let movieSelected = await Movie.findAll({
-            where: {
-                title: movie
-            }
-        })
+    let movieSelected = await Movie.findAll({
+      where: {
+        title: movie,
+      },
+    });
 
-        await newCharacter.addMovies(movieSelected)
-        
-        res.status(201).send(newCharacter)
-        
-    } catch (error) {
-        return res.status(500).json({message: error.message})
-    }
-}
+    await newCharacter.addMovies(movieSelected);
+
+    res.status(201).send(newCharacter);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const updateCharacter = async (req, res) => {
-    try {
+  try {
+    const { id } = req.params;
+    const { name, image, age, weight, story, movies } = req.body;
 
-        const {id} = req.params;
-        const {name, image, age, weight, story, movies} = req.body;
+    const updated = await Character.findByPk(id);
 
-        const updated = await Character.findByPk(id)
+    name ? (updated.name = name) : null;
+    image ? (updated.image = image) : null;
+    age ? (updated.age = age) : null;
+    weight ? (updated.weight = weight) : null;
+    story ? (updated.story = story) : null;
+    movies ? (updated.movies = movies) : null;
 
-        name ? updated.name = name : null;
-        image ? updated.image = image : null;
-        age ? updated.age = age : null;
-        weight ? updated.weight = weight : null;
-        story ? updated.story = story : null;
-        movies ? updated.movies = movies : null;
+    await updated.save();
 
-        await updated.save()
-
-        res.json({updated})
-        
-    } catch (error) {
-        return res.status(500).json({message: error.message})
-    }
-}
+    res.json({ updated });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const deleteCharacter = async (req, res) => {
-    try {
+  try {
+    const { id } = req.params;
 
-        const {id} = req.params;
+    await Character.destroy({
+      where: {
+        id,
+      },
+    });
 
-        await Character.destroy({
-            where: {
-                id
-            }
-        })
-
-        res.sendStatus(204)
-        
-    } catch (error) {
-        return res.status(500).json({message: error.message})
-    }
-}
+    res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 export const getCharacter = async (req, res) => {
-    try {
+  try {
+    const { id } = req.params;
 
-        const {id} = req.params;
+    const character = await Character.findOne({
+      where: {
+        id,
+      },
+      include: {
+        model: Movie,
+        attributes: ["title"],
+        through: {
+          attributes: [],
+        },
+      },
+    });
 
-        const character = await Character.findOne({
-            where: {
-                id
-            },
-            include: {
-                model: Movie,
-                attributes: ['title'],
-                through: {
-                    attributes: []
-                  }
-            }
-
-        })
-
-        res.json(character)
-        
-    } catch (error) {
-        return res.status(500).json({message: error.message})
-    }
-}
+    res.json(character);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
