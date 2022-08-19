60f21e191069a4f596d098dac2cf043edee03547
camila <camilafkapp@gmail.com>
2022-08-19 02:13:15 -0300

    continue login

--- src/models/User.js
+++ src/models/User.js
@@ -13,7 +13,7 @@ const User = sequelize.define('user', {
         allowNull: false,
         unique: true
     },
-    password: {
+    passwordDB: {
         type: Sequelize.STRING,
         allowNull: false
     }
